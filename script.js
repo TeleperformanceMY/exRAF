8507ca5116994a133aa008417d996bc3978c3821
TeleperformanceMY <167857273+TeleperformanceMY@users.noreply.github.com>
2025-05-05 12:03:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -546,7 +546,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         if (job) {
             const baseUrl = job['Evergreen link'].split('?')[0];
-            const referralUrl = `${baseUrl}?exref=friend&name=${name}%7Cphone=${phone}%7Cemail=${email}`;
+            const referralUrl = `${baseUrl}?iis=exRAF&iisn=friend&name=${name}%7Cphone=${phone}%7Cemail=${email}`;
             
             elements.referralLink.value = referralUrl;
             generateQRCode(referralUrl);
